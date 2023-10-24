e3329e74116e0c4ded975969b229e7cebd2f7cfc
SolalDFG <144117691+SolalDFG@users.noreply.github.com>
2023-10-24 12:26:00 +0200

    Add files via upload

---
+++ tp8/tp8ex1.js
@@ -0,0 +1,19 @@
+function addition(){ 
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)+Number(b);
+
+
+    t3.value = c;
+
+}
+
+
+  function multiplication(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)*Number(b);
+
+    t3.value = c;
+
+  }
